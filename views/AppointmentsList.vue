278207b894ae179679c5521a9d02d158bb742922
Laith Altawil <tdmer2003@gmail.com>
2025-06-19 17:27:45 +0300

    edited

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -74,7 +74,7 @@ export default {
       console.log("appointmentId:", cleanAppointment.appointmentId);
       console.log(" appointmentId (direct):", appointment.appointmentId);
 
-      const url = `https://u1lesqctck.execute-api.us-east-1.amazonaws.com/ServerLess/Appointments/${appointment.appointmentId}`;
+      const url = `https://u1lesqctck.execute-api.us-east-1.amazonaws.com/ServerLess/Appointments/${Appointments.appointmentId}`;
 
       const payload = { status: newStatus };
 
